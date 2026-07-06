81548f3736e1912a492ec18d45bfdb6111930808
LottieOrton <lottieorton11@gmail.com>
2026-07-06 22:00:53 +1000

    adds api call function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import { getBooksBySearchTerm } from "./services/books-service";
 
 function App() {
+  getBooksBySearchTerm("flowers").then((data) => console.log(data.items));
+
   return (
     <main>
       <Header />
---
+++ src/services/books-service.js
@@ -0,0 +1,17 @@
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+
+export const getBooksBySearchTerm = async (term) => {
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${term}&key=${apiKey}`,
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch books");
+    }
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    console.error(err.message);
+  }
+};
