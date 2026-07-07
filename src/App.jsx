bbc363c0e41acc11d44dee74942fdd0727a78ac4
LottieOrton <lottieorton11@gmail.com>
2026-07-07 21:51:34 +1000

    adds remaining dummy components with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
-import { getBooksBySearchTerm } from "./services/books-service";
+import BookList from "./components/BookList/BookList";
+// import { getBooksBySearchTerm } from "./services/books-service";
 
 function App() {
-  getBooksBySearchTerm("flowers").then((data) => console.log(data.items));
+  //getBooksBySearchTerm("flowers").then((data) => console.log(data.items));
 
   return (
     <main className="main">
       <Header />
       <SearchBar />
+      <BookList />
     </main>
   );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,7 +3,7 @@ import classes from "./SearchBar.module.scss";
 export default function SearchBar() {
   return (
     <form className={classes.form}>
-      <i className={classes.searchIcon + " fa-solid fa-magnifying-glass"}></i>
+      <i className={classes.icon + " fa-solid fa-magnifying-glass"}></i>
       <input
         className={classes.input}
         type="text"
