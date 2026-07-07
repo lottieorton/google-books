c3260cb1dd0a62b5f671b1dc877413a1c55fb0aa
LottieOrton <lottieorton11@gmail.com>
2026-07-07 23:14:53 +1000

    adds searchbar state functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.scss";
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
 import BookList from "./components/BookList/BookList";
-// import { getBooksBySearchTerm } from "./services/books-service";
+import { getBooksBySearchTerm } from "./services/books-service";
 
 function App() {
-  //getBooksBySearchTerm("flowers").then((data) => console.log(data.items));
+  // getBooksBySearchTerm("flowers").then((data) => console.log(data.items));
+  const [searchTerm, setSearchTerm] = useState("");
+  const onSearch = (value) => setSearchTerm(value);
 
   return (
     <main className="main">
       <Header />
-      <SearchBar />
+      <SearchBar onSearch={onSearch} />
       <BookList />
     </main>
   );
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import classes from "./SearchBar.module.scss";
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(input);
+    setInput("");
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleSubmit}>
       <i className={classes.icon + " fa-solid fa-magnifying-glass"}></i>
       <input
         className={classes.input}
         type="text"
         placeholder="Search books..."
+        value={input}
+        onChange={handleChange}
       />
       <button className={classes.btn}>Go</button>
     </form>
